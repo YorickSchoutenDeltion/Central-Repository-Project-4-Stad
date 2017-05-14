094224752f7f6f24f9803ced60e6324c909447fd
StephanHoldingDeltion <stephan@holdingsnel.nl>
2017-05-14 16:54:49 +0200

    Pickups & ResourceManager + Trashcan.max

---
+++ Project 4 Stad GMD1B/Assets/[Assets Guy]/Scripts/DEV_Pickups.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class DEV_Pickups : MonoBehaviour {
+
+    public GameObject medKit;
+    public GameObject ammoBox;
+    public GameObject player;
+
+    public ResourceManager resources;
+
+    public int medKitIncrease;
+    public int ammoIncrease;
+
+    private void Awake() {
+
+        player = GameObject.Find("Player");
+        resources = player.GetComponent<ResourceManager>();
+    }
+
+    /* Checks if the player walks against a item thats qualified as pickup
+    if so, destroys the object it was colliding with and adds an amount to one
+    of the elements of the resource manager*/
+
+    private void OnCollisionEnter(Collision c) {
+
+        if (c.gameObject == medKit) {
+            Destroy(c.gameObject);
+            resources.medKits += medKitIncrease;
+
+            if (c.gameObject == ammoBox) {
+                Destroy(c.gameObject);
+                resources.ammo += ammoIncrease;
+            }
+        }
+    }
+}
---
+++ Project 4 Stad GMD1B/Assets/[Assets Stephan]/Scripts/DEV_ResourceManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DEV_ResourceManager : MonoBehaviour {
+
+    public int health;
+    public int medkits;
+    public int maximumHealth;
+
+    public int ammo;
+
+    public void HealthDecrease(int amount)
+    {
+        health -= amount;
+        if (health <= 0)
+        {
+            //gameover
+        }
+    }
+
+    public void HealthIncrease(int amount)
+    {
+        health += amount;
+        if (health > maximumHealth)
+        {
+            health = maximumHealth;
+        }
+    }
+
+    public void AmmoIncrease(int amount)
+    {
+        ammo += amount;
+    }
+}
