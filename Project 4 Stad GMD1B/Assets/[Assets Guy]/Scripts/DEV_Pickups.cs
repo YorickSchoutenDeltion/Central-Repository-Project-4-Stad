ee9aeb02c9fa2b540e438bb21ff548c9787b09fd
GuyVanDerMeulenDeltion <guy_meulen@live.nl>
2017-05-19 09:56:20 +0200

    Editted Dev_Pickups

--- Project 4 Stad GMD1B/Assets/[Assets Guy]/Scripts/DEV_Pickups.cs
+++ Project 4 Stad GMD1B/Assets/[Assets Guy]/Scripts/DEV_Pickups.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
 
 public class DEV_Pickups : MonoBehaviour {
 
@@ -6,13 +7,21 @@ public class DEV_Pickups : MonoBehaviour {
     public GameObject ammoBox;
     public GameObject player;
 
+    public List<GameObject> mechanicalPartsNeeded = new List<GameObject>();
+    public List<GameObject> mechanicalPartsGotten = new List<GameObject>();
+    public List<GameObject> weapons = new List<GameObject>();
+
+    public List<string> tags = new List<tags>();
+
     public ResourceManager resources;
 
-    public int medKitIncrease;
+    public int healthIncrease;
     public int ammoIncrease;
+    public int tagID;
+
+    public bool allPartsScavenged;
 
     private void Awake() {
-
         player = GameObject.Find("Player");
         resources = player.GetComponent<ResourceManager>();
     }
@@ -21,15 +30,32 @@ public class DEV_Pickups : MonoBehaviour {
     if so, destroys the object it was colliding with and adds an amount to one
     of the elements of the resource manager*/
 
+    public void Update() {
+        if(mechanicalPartsGotten == mechanicalPartsNeeded) {
+            allPartsScavenged = true;
+        }
+    }
+
+    /* NOTE: Tags still needs to be added to the string list */
+
     private void OnCollisionEnter(Collision c) {
-
-        if (c.gameObject == medKit) {
+        if (c.gameObject == tags[0]) {
             Destroy(c.gameObject);
-            resources.medKits += medKitIncrease;
+            resources.health += healthIncrease;
 
-            if (c.gameObject == ammoBox) {
+            if (c.gameObject == tags[1]) {
                 Destroy(c.gameObject);
                 resources.ammo += ammoIncrease;
+            }
+
+            if (c.gameObject == tags[2]) {
+                mechanicalPartsGotten.Add(c.gameObject);
+                Destroy(c.gameObject);
+            }
+
+            if (c.gameObject == tags[3]) {
+                weapons.Add(c.gameObject);
+                Destroy(c.gameObject);
             }
         }
     }
